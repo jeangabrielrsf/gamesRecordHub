0bba37b691661d83b6d3e0c08dc3cb0511a6ba99
Jean Frusca <jeang96@gmail.com>
2024-02-14 19:56:12 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import Header from './components/Header.vue';
+  export default {
+    components: {
+      Header
+    }
+  }
+  
+</script>
+
+
+<template>
+  <v-app>
+    <Header />
+  </v-app>
+
+</template>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+
+<script>
+    export default {
+        name: "Header",
+        data() {
+            return {
+                title: "Application Navigation"
+            }
+        }
+    }
+
+</script>
+
+<template>
+    <v-app-bar width="100%">
+        <v-app-bar-nav-icon>
+        </v-app-bar-nav-icon>
+        <v-app-bar-title>
+            {{ this.title }}
+        </v-app-bar-title>
+    </v-app-bar>
+
+   
+</template>
